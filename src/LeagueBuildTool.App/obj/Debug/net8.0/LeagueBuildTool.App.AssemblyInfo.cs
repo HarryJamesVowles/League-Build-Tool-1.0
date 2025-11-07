e192036c83c87de73b3843de6000b9ff9f7cc3fc
HarryJamesVowles <16vowleshar@gmail.com>
2025-11-07 12:09:28 +0000

    chore: update assembly informational version and project file paths to use consistent casing

--- src/LeagueBuildTool.App/obj/Debug/net8.0/LeagueBuildTool.App.AssemblyInfo.cs
+++ src/LeagueBuildTool.App/obj/Debug/net8.0/LeagueBuildTool.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeagueBuildTool.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d6053588b4f204b4858c3c3aace01e3053aeca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c013e987d929558cebfd7b73285dc10b0b2204")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeagueBuildTool.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeagueBuildTool.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
